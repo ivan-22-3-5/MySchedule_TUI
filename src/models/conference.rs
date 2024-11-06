517159e4981dbac02d65e352ccb80306a8f91868
Ivan Mihun <vsk76090@gmail.com>
2024-11-06 17:17:28 +0100

    feat: add conference struct

---
+++ src/models/conference.rs
@@ -0,0 +1,12 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Conference {
+    pub title: String,
+    pub link: String,
+    pub start_time: String,
+    pub duration: u8,
+    pub password: Option<String>,
+    pub autostart_permission: bool,
+    pub week: u8,
+}
