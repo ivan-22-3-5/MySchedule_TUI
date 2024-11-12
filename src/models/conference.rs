b24e19a7d184e492dca41c99436bd346f3d71851
Ivan Mihun <vsk76090@gmail.com>
2024-11-12 13:57:18 +0100

    feat: impl Display trait on Conference

--- src/models/conference.rs
+++ src/models/conference.rs
@@ -1,6 +1,8 @@
 use serde::{Deserialize, Serialize};
+use std::fmt;
+use std::fmt::Display;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Conference {
     pub title: String,
     pub link: String,
@@ -9,4 +11,23 @@ pub struct Conference {
     pub password: Option<String>,
     pub autostart_permission: bool,
     pub week: u8,
+}
+
+impl Display for Conference {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "Conference Details:\nTitle: {}\nLink: {}\nStart Time: {}\nDuration: {} hours\n{}Autostart Permission: {}\nWeek: {}",
+            self.title,
+            self.link,
+            self.start_time,
+            self.duration,
+            match &self.password {
+                Some(p) => format!("Password: {}\n", p),
+                None => "Password: not set\n".to_string(),
+            },
+            if self.autostart_permission { "Yes" } else { "No" },
+            self.week
+        )
+    }
 }
