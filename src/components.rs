2fb88ea45249c6ab2f51711fc374f43343bd07ab
Ivan Mihun <vsk76090@gmail.com>
2024-11-24 19:52:14 +0100

    fix: remove nonexistent module def

--- src/components.rs
+++ src/components.rs
@@ -10,7 +10,6 @@ use crate::{action::Action, config::Config, tui::Event};
 
 pub mod fps;
 pub mod home;
-mod counter;
 
 /// `Component` is a trait that represents a visual and interactive element of the user interface.
 ///
